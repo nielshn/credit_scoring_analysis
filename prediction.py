6ff226a78c69e4d1a70819b1cc6b59b6207b3006
nielshn <niel.shn08@gmail.com>
2025-05-13 14:15:28 +0700

    Credit Scoring Analytic

---
+++ prediction.py
@@ -0,0 +1,19 @@
+import joblib
+
+model = joblib.load("model/gboost_model.joblib")
+result_target = joblib.load("model/encoder_target.joblib")
+
+
+def prediction(data):
+    """Making Prediction
+
+    Args:
+        data (Pandas DataFrame): DataFrame that contain all the preprocessed data
+
+    Returns:
+        str: Prediction result (Good, Standard, or Poor)
+    """
+
+    result = model.predict(data)
+    final_result = result_target.inverse_transform(result)[0]
+    return final_result
